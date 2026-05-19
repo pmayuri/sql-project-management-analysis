1faefdab6e70c0d4260b3dac2f781854c466f59f
MAYURI PRAJAPATI <pmayuri124@gmail.com>
2026-05-20 01:17:28 +0530

    Update dataset.sql

--- dataset.sql
+++ dataset.sql
@@ -38,3 +38,35 @@ INSERT INTO employees VALUES
 (1, 'Amit', 'Operations'),
 (2, 'Sneha', 'Product'),
 (3, 'Rahul', 'Engineering');
+
+
+-- Total hours per project
+
+SELECT p.project_name,
+       SUM(t.hours_spent) AS total_hours
+FROM projects p
+JOIN tasks t
+ON p.project_id = t.project_id
+GROUP BY p.project_name;
+
+
+-- Completed tasks
+
+SELECT p.project_name,
+       COUNT(*) AS completed_tasks
+FROM projects p
+JOIN tasks t
+ON p.project_id = t.project_id
+WHERE status='Completed'
+GROUP BY p.project_name;
+
+
+-- High workload projects
+
+SELECT p.project_name,
+       SUM(t.hours_spent)
+FROM projects p
+JOIN tasks t
+ON p.project_id=t.project_id
+GROUP BY p.project_name
+HAVING SUM(t.hours_spent)>20;
